4b285ffe18bb18cc78af8854ebfecb084fdf1992
prateek-ml <prateekb1912@gmail.com>
2021-11-17 17:26:28 +0530

    Added template files for the home and room pages

--- base/views.py
+++ base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Python'},
+    {'id': 2, 'name': 'Java'},
+    {'id': 3, 'name': 'Node.JS'}
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
