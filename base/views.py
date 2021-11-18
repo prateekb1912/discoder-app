adf4368cb5f074573740f15cd736c688109a2361
prateek-ml <prateekb1912@gmail.com>
2021-11-18 17:09:04 +0530

    Add a form to create new rooms in the app

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
@@ -10,3 +11,15 @@ def room(request, id):
     room = Room.objects.get(id = id)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def create_room(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
