26c7398a9b84aff895f956e69df74368260ef809
prateek-ml <prateekb1912@gmail.com>
2021-11-18 16:04:44 +0530

    Change templates structure and URLs

--- base/views.py
+++ base/views.py
@@ -8,7 +8,13 @@ rooms = [
 
 def home(request):
     context = {'rooms':rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, id):
+    room = None
+    for i in rooms:
+        if i['id'] == int(id):
+            room  = i
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
